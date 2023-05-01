2217a80c29b86c5ebc305b08e5da13c063c9ea2e
evegand <a19310213@ceti.mx>
2023-05-01 11:54:42 -0600

    P5 print change

--- Tecnologías Emergentes/practica5.py
+++ Tecnologías Emergentes/practica5.py
@@ -3,7 +3,7 @@ list = [int(value) for value in values]
 
 # a) Sublista a la mitad
 sublist = list[int(len(list)/2)-1:int(len(list)/2)+1]
-print(f"a) {sublist}")
+print(f"a) Sublista con 2 valores centrales\n{sublist}")
 
 # b) Primer y ultimo elemento
-print(f"b) {list[0]}, {list[-1]}",)
+print(f"b) Primer y ultimo elemento\n{list[0]}, {list[-1]}",)
